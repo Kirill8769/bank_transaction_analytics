f2fc4a63b6332f523c9e35793244c5553e76c52e
Kirill8769 <tkkrozn@gmail.com>
2023-11-28 12:35:23 +0300

    add get_price_stocks, get_price_currencies

--- src/utils.py
+++ src/utils.py
@@ -1,25 +1,24 @@
 import logging.config
 import os
+from datetime import datetime
 
 
 def my_logger():
-    log_file = os.path.join("logging_config.ini")
+    log_file = os.path.join("!=log_config.ini")
     if os.path.isfile(log_file):
-        print("logger_file")
         logging.config.fileConfig(log_file)
         my_logger = logging.getLogger("my_logger")
-        return my_logger
     else:
-        print("logger_console")
         my_logger = logging.getLogger("logger")
         my_logger.setLevel("DEBUG")
-        file_handler = logging.FileHandler(filename="logging.log", mode="w", encoding="UTF-8")
+        log_name = os.path.join("logs", f'log_{datetime.today().strftime("%d_%m_%Y")}.log')
+        file_handler = logging.FileHandler(filename=log_name, mode="a", encoding="UTF-8")
         file_formatter = logging.Formatter(
             "[%(asctime)s] %(levelname)s %(filename)s-%(funcName)s: %(message)s", datefmt="%d.%m.%Y-%H:%M:%S"
         )
         file_handler.setFormatter(file_formatter)
         my_logger.addHandler(file_handler)
-        return my_logger
+    return my_logger
     
 
 logger = my_logger()
--- src/views.py
+++ src/views.py
@@ -5,43 +5,103 @@ import pandas as pd
 from datetime import datetime
 
 import requests
+from dotenv import load_dotenv
 
 from utils import logger
 
+load_dotenv()
+API_MARKETSTACK = os.getenv("API_MARKETSTACK")
 
-def foo(user_date: str):
-    if not isinstance(user_date, str):
-        raise TypeError("Передан неверный тип данных, ожидается str")
-    pattern = re.compile(r'\b\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\b')
-    date_str = pattern.search(user_date)
-    if not date_str:
-        raise ValueError("Передан неверный формат даты, ожидается YYYY-MM-DD HH:MM:SS")
-    
-    end_date = datetime.strptime(date_str[0], "%Y-%m-%d %H:%M:%S")
-    start_date = datetime(end_date.year, end_date.month, 1)
-    print(end_date, start_date)
-    print(get_time_of_day(end_date))
 
-    response = requests.get("https://www.cbr-xml-daily.ru/daily_json.js")
-    result = json.loads(response.text)
-    #print(result)
+def get_range_dates(user_date: str):
+    start_date = None
+    end_date = None
+    try:
+        if not isinstance(user_date, str):
+            raise TypeError("Передан неверный тип данных, ожидается str")
+        pattern = re.compile(r'\b\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\b')
+        date_str = pattern.search(user_date)
+        if not date_str:
+            raise ValueError("Передан неверный формат даты, ожидается YYYY-MM-DD HH:MM:SS")   
+        end_date = datetime.strptime(date_str[0], "%Y-%m-%d %H:%M:%S")
+        start_date = datetime(end_date.year, end_date.month, 1)
+    except TypeError as type_ex:
+        logger.error(f"{type_ex.__class__.__name__}: {type_ex}")
+    except ValueError as val_ex:
+        logger.error(f"{val_ex.__class__.__name__}: {val_ex}")
+    except Exception as ex:
+        logger.debug(f"{ex.__class__.__name__}: {ex}")
+    finally:
+        return start_date, end_date
 
-    
+
+
+def get_tickers_dict():
     settings_path = os.path.join("user_settings.json")
     if os.path.isfile(settings_path):
         with open(settings_path, encoding="UTF-8") as file:
             user_settings = json.load(file)
-            print(user_settings)
+        tickers_currencies = {key: 0 for key in user_settings["user_currencies"]}
+        tickers_stocks = {key: 0 for key in user_settings["user_stocks"]}
+        return tickers_currencies, tickers_stocks
     else:
-        # log , raise
-        pass
+        print("tickers error")
+        return None, None
+
+
+def get_price_stocks(user_stocks: dict) -> dict:
+    try:
+        if not isinstance(user_stocks, dict):
+            raise TypeError("Передан неверный формат, ожидается словарь с акциями")
+        if user_stocks:
+            symbols = ",".join(user_stocks.keys())
+            params = {"access_key": API_MARKETSTACK, "symbols": symbols}
+            url = f"http://api.marketstack.com/v1/intraday/latest"
+            response = requests.get(url, params)
+            if not response.status_code == 200:
+                raise ConnectionError("Ошибка подключения")
+            json_data = response.json()
+            for stock in json_data["data"]:
+                user_stocks[stock["symbol"]] = stock["last"]
+    except TypeError as type_ex:
+        logger.error(f"{type_ex.__class__.__name__}: {type_ex}")
+    except ConnectionError as conn_ex:
+        logger.error(f"{conn_ex.__class__.__name__}: {conn_ex}")
+    except Exception as ex:
+        logger.debug(f"{ex.__class__.__name__}: {ex}", exc_info=True)
+    finally:
+        return user_stocks
+
+
+
+def get_price_currencies(user_currencies: dict) -> dict:
+    try:
+        if not isinstance(user_currencies, dict):
+            raise TypeError("Передан неверный формат, ожидается словарь с валютами")
+        if user_currencies:
+            url = "https://www.cbr-xml-daily.ru/daily_json.js"
+            response = requests.get(url)
+            if not response.status_code == 200:
+                raise ConnectionError("Ошибка подключения")
+            currency_info = response.json()
+            for currency in user_currencies:
+                user_currencies[currency] = currency_info["Valute"][currency]["Value"]
+    except TypeError as type_ex:
+        logger.error(f"{type_ex.__class__.__name__}: {type_ex}")
+    except ConnectionError as conn_ex:
+        logger.error(f"{conn_ex.__class__.__name__}: {conn_ex}")
+    except Exception as ex:
+        logger.debug(f"{ex.__class__.__name__}: {ex}", exc_info=True)
+    finally:
+        return user_currencies
+
     
-    filter_result = []
-    for k, v in result["Valute"].items():
-        if k in user_settings["user_currencies"] or k in user_settings["user_stocks"]:
-            print(k, v["Value"])
+    
 
 
+currencies, stocks = get_tickers_dict()
+# get_price_stocks(stocks)
+get_price_currencies(currencies)
 
 
 def get_operations(start_date, stop_date):
@@ -55,7 +115,7 @@ def get_operations(start_date, stop_date):
 
 
 
-get_operations()
+#get_operations()
 
 
 
@@ -89,7 +149,7 @@ def get_time_of_day(user_date: datetime) -> str:
 
 
 
-foo("2023-12-22 13:13:13")
+get_range_dates("2023-12-22 13:13:13")
 
 
 
