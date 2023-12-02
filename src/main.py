1474ca3f18411e25ced7b0f7609e938761cf7572
kirill8769 <tkkrozn@gmail.com>
2023-12-02 22:03:56 +0300

    add test_reports.py and write tests

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ def main() -> None:
     result_top_cashback = get_categories_of_increased_cashback(df_operations, 2021, 2)
     print(result_top_cashback)
     spending_by_category(df_operations, "Супермаркеты", "2019-01-22 11:11:11")
+    print(spending_by_category)
 
 
 if __name__ == "__main__":
---
+++ tests/test_reports.py
@@ -0,0 +1,27 @@
+import pytest
+import pandas as pd
+
+from src.reports import spending_by_category
+
+
+@pytest.fixture()
+def user_transactions():
+    transactions = {
+        "Дата платежа": ["05.11.2022", "20.11.2022", "01.01.2023", "01.06.2023"],
+        "Категория": ["Мебель", "Супермаркет", "Мебель", "Мебель"]
+    }
+    return pd.DataFrame(transactions)
+
+
+def test_spending_by_category_correct_answer(user_transactions):
+    expected = pd.DataFrame(
+        {"Дата платежа": [pd.to_datetime("01.06.2023", format="%d.%m.%Y")],
+         "Категория": ["Мебель"]})
+    result_1 = spending_by_category(user_transactions, "Мебель", "2023-06-22 00:00:00")
+    pd.testing.assert_frame_equal(result_1.reset_index(drop=True), expected)
+
+
+def test_spending_by_category_incorrect_args(user_transactions):
+    assert spending_by_category("incorrect", "Мебель", "2023-06-22 00:00:00") == None
+    assert spending_by_category(user_transactions, 123, "2023-06-22 00:00:00") == None
+    assert spending_by_category(user_transactions, "Мебель", "2023-06-22") == None
